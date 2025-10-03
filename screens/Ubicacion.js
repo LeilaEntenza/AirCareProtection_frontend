75c9f6dd8edde320ba05377ac1e2cc1ff77213bb
Android Studio <devandroid@ort>
2025-10-03 10:19:13 -0300

    Maps con marker

--- screens/Ubicacion.js
+++ screens/Ubicacion.js
@@ -1,60 +1,29 @@
-import React, { useState} from 'react';
-import { ScrollView, View, Text, StyleSheet, Image, TextInput, Pressable, Switch} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker} from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-export default function Ubicación({ navigation }) {
+export default function Ubicacion() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: -34.605950918242094,
+    longitude: -58.435351633822265,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.05,
+  });
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.scrollContent}>
-      <Text style={{ fontSize: 18, marginTop: 20, marginBottom: 10 }}>Ubicación actual</Text>
-      <Image
-      source={{ uri: 'https://www.esedeerre.com/wp-content/uploads/2012/06/geolocalizacion-google-maps.jpg' }}
-      style={styles.ubicacionMapa}
-      resizeMode="contain"
-      />
-      <Text style={{ fontSize: 18, marginTop: 20, marginBottom: 10 }}>Ubicación del dispositivo</Text>
-      <Image
-      source={{ uri: 'https://www.esedeerre.com/wp-content/uploads/2012/06/geolocalizacion-google-maps.jpg' }}
-      style={styles.ubicacionMapa}
-      resizeMode="contain"
-      />
-    </ScrollView>
+    <View style={styles.container}>
+      <MapView style={styles.map} region={mapRegion}>
+        <Marker coordinate={mapRegion} title='Dispositivo'/>
+      </MapView>
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: '100%',
-    },
-    scrollContent: {
-      flexGrow: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingVertical: 12,
-    },
-    ubicacionMapa: {
-    width: '100%',     
-    height: 200, 
-    marginTop: 10,
-    alignSelf: 'center', 
-    overflow: 'hidden',  
-    },
-    input: {
-        width: '50%',
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        paddingHorizontal: 10,
-        backgroundColor: 'white',
-    },
-    boton: {
-        backgroundColor: '#636891',
-        width: '40%',
-        padding: 10,
-        borderRadius: 6,
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    textButton: {
-        color: 'white',
-        fontSize: 16,
-    },
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
 });
